a36572efd5b2e6bd14d9ab184dc599a5268de1ed
Ariana <152944814+aam214@users.noreply.github.com>
2025-01-12 18:49:36 -0600

    Decreased font-size for quiz-choice. Changed the function name of triviaQuestions to randomTriviaQuestions for better understanding. Created function of showQuestion to render the current question and append each list element. Used the forEach method to iterate through each option from questions.js.  Removed hardcoding of quiz-choice list elements from HTML file.

--- JS/trivia.js
+++ JS/trivia.js
@@ -1,11 +1,31 @@
+const quizQuestion = document.querySelector(".quiz-question");
+const quizChoices = document.querySelector(".quiz-choices");
+
 let triviaSubject = "history";
 
-const triviaQuestions = () => {
-  const subjectQuestions = questions.find(cat => 
-  cat.category.toLowerCase() === triviaSubject.toLowerCase()).questions;
+//Get question based off category chosen
+const randomTriviaQuestion = () => {
+const subjectQuestions = questions.find(cat => 
+cat.category.toLowerCase() === triviaSubject.toLowerCase()).questions;
 
-
-  const randomQuestion = subjectQuestions[Math.floor(Math.random() * subjectQuestions.length)];
-  console.log(randomQuestion); 
+const randomQuestion = subjectQuestions[Math.floor(Math.random() * subjectQuestions.length)];
+return randomQuestion;
 }
-triviaQuestions();
+
+//Show question
+
+const showQuestion = () => {
+  const questionNow = randomTriviaQuestion();
+  if(!questionNow) return;
+  quizQuestion.textContent = questionNow.question;
+
+  questionNow.options.forEach(option => {
+  const li = document.createElement("li");
+  li.classList.add("quiz-choice");
+  li.textContent = option;
+  quizChoices.appendChild(li);
+  });
+
+}
+
+showQuestion();
