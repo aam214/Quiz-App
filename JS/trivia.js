5a28839d35b1c66935c6a7582e55a88bcdb5de57
Ariana <152944814+aam214@users.noreply.github.com>
2025-01-14 18:22:53 -0600

    Added an event listener when the list element is clicked in trivia.js. Edited .forEach method to pass two arguments. Set quizChoices to an empty string in showQuestion function to get rid of previous question list elements. Edited color for .wrong in CSS. In questions.js changed correctAnswer to correctChoice. Fixed space typo in querySelector for ".fa-solid.fa...." Added an || empty array to randomTriviaQuestion function.

--- JS/trivia.js
+++ JS/trivia.js
@@ -1,33 +1,48 @@
 const quizQuestion = document.querySelector(".quiz-question");
 const quizChoices = document.querySelector(".quiz-choices");
-const nextQuestion = document.querySelector(".fa-solid fa-arrow-right");
+const nextQuestion = document.querySelector(".fa-solid.fa-arrow-right");
 
 let triviaSubject = "history";
+
+let questionNow = null;
+
 
 //Get question based off category chosen
 const randomTriviaQuestion = () => {
 const subjectQuestions = questions.find(cat => 
-cat.category.toLowerCase() === triviaSubject.toLowerCase()).questions;
+cat.category.toLowerCase() === triviaSubject.toLowerCase()).questions
+|| [];
 
 const randomQuestion = subjectQuestions[Math.floor(Math.random() * subjectQuestions.length)];
 return randomQuestion;
 }
 
-//Show question
 
+
+const userChoice = (option, answerIndex) => {
+  const isCorrect = questionNow.correctChoice ===
+  answerIndex;
+  option.classList.add(isCorrect ? 'correct' : 'wrong');
+}
+
+
+//Show question
 const showQuestion = () => {
-  const questionNow = randomTriviaQuestion();
+  questionNow = randomTriviaQuestion();
   if(!questionNow) return;
+  quizChoices.innerHTML = '';
   quizQuestion.textContent = questionNow.question;
 
-  questionNow.options.forEach(option => {
+  questionNow.options.forEach((option, index) => {
   const li = document.createElement("li");
   li.classList.add("quiz-choice");
   li.textContent = option;
   quizChoices.appendChild(li);
-  });
 
-}
+  //Choose option
+  li.addEventListener("click", () => userChoice(li, index));
+  });
+  };
 showQuestion();
 
 nextQuestion.addEventListener("click", showQuestion);
