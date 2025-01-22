bf036802901d38718c962e7a970f40439c77c6fc
Ariana <152944814+aam214@users.noreply.github.com>
2025-01-22 17:04:34 -0600

    Added new objects of category  of popculture and question with an array of questions in the questions.js.

--- JS/questions.js
+++ JS/questions.js
@@ -53,4 +53,58 @@ correctChoice: 1,
 },
 ]
 }
+{category: "popculture",
+  questions: [
+    {
+    question: "What year was the release of Thriller by Michael Jackson?",
+    options: ["1990", "1972", "1982","1998", "1950"],
+    correctChoice: 2,  
+  },
+  {
+  question: "What is the name of the famous Christmas movie featuring Macaulay Caulken?",
+  options: ["Home Alone", "Jingle All The Way", "White Christmas","Elf", "Bug's Life"],
+  correctChoice: 0,  
+  },
+  {
+    question: "Super Mario is a platform game series created by which company?",
+    options: ["Nintendo", "Play Station", "Sega", "Game Boy", "Atari"],
+    correctChoice: 0,
+  },
+  {
+    question: "What year was the first Hershey bar sold?",
+    options: ["1920", "1900", "1980","2000", "1930"],
+    correctChoice: 1,  
+  },
+  {
+    question: "What color is not on the Pepsi logo?",
+    options: ["Blue", "Red", "Orange","White", "Black"],
+    correctChoice: 2,
+  },
+  {
+    question: "Which state is known for it's oranges?",
+    options: ["California", "Kentucky", "Hawaii","Florida", "Nebraska"],
+    correctChoice: 3,  
+  },
+  {
+    question: "What month is St. Patrick's Day?",
+    options: ["March", "April", "February", "May", "December"],
+    correctChoice: 0,  
+  },
+  {
+    question: "Which English girl group started in the 90s?",
+    options: ["En Vogue", "Salt-N-Peppa", "*NSYNC","Spice Girls", "Ace of Base"],
+    correctChoice: 3,  
+  },
+  {
+    question: "Which doll was introduced in 1959",
+    options: ["Polly Pocket", "Barbie", "Cabbage Patch","Legos", "American Girl"],
+    correctChoice: 1,  
+  },
+  {
+  question: "What series is Pikachu in?",
+  options: ["Digimon", "Seasame Street", "Yu-Gi-Oh!","Power Puff Girls", "Pokemon"],
+  correctChoice: 4,  
+  },
+  ]
+  }
 ];
